c805ac0f7b4a98e5d23ad4cf0f8381d9147c8357
Piotr Podolski <piotr.podolski@schibsted.pl>
2016-05-31 00:51:33 +0200

    further generator changes

---
+++ Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpener : MonoBehaviour {
+	GameObject doors;
+	private bool closed = true;
+
+	// Use this for initialization
+	void Start () {
+		Transform t = transform;
+		foreach(Transform tr in t)
+		{
+			if(tr.tag.Equals("Door"))
+			{
+				doors = tr.gameObject;
+				return;
+			}
+		}
+	}
+
+	void toggleDoors() {
+		Vector3 hinge = new Vector3 (transform.position.x, transform.position.y, transform.position.z + Config.DOOR_WIDTH / 2);
+		if (!closed) {
+			doors.transform.RotateAround (hinge, new Vector3 (0, 1, 0), 90);
+		} else {
+			doors.transform.RotateAround (hinge, new Vector3 (0, 1, 0), -90);
+		}
+		closed = !closed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.E)) {
+			GameObject player = GameObject.FindGameObjectsWithTag("Player")[0];
+			var distance = Vector3.Distance(transform.position, player.transform.position);
+			if (distance < 5f) {
+				toggleDoors();
+			}
+		}
+	}
+}
